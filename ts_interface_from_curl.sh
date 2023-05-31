d14b503d22f6ca55266899cdd2df9af27e21816b
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-05-31 11:27:09 -0400

    Add -f and -i arguments and use those to name output file

--- ts_interface_from_curl.sh
+++ ts_interface_from_curl.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
 # URL to retrieve response from
-URL="https://statsapi.mlb.com/api/v1/attendance"
+URL="https://example.com/api/endpoint"
+
+# Default values
+FILENAME=""
+INTERFACE_NAME=""
+
+# Parse command-line arguments
+while getopts ":f:i:" opt; do
+  case $opt in
+    f)
+      FILENAME=$OPTARG
+      ;;
+    i)
+      INTERFACE_NAME=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
 
 # Temporary file to store response
 TMP_FILE="/tmp/response.json"
@@ -15,17 +35,53 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-# Generate Unix timestamp
-TIMESTAMP=$(date +%s)
+# Generate TypeScript interface from nested JSON recursively
+generate_interface() {
+  local prefix=$1
+  local json_file=$2
+  local interface_name=$3
 
-# Generate TypeScript interface
-INTERFACE=$(jq -r 'to_entries | map("  \"" + .key + "\": " + (.value|type)) | join(",\n")' "$TMP_FILE")
+  local json_type=$(jq -r 'type' "$json_file")
+
+  case $json_type in
+    "object")
+      local properties=$(jq -r 'keys | .[]' "$json_file")
+      echo "export interface $interface_name {" >> "$INTERFACE_FILE"
+      while IFS= read -r property; do
+        local property_type=$(jq -r ".$property | type" "$json_file")
+        local nested_interface_name="${interface_name}_${property}"
+
+        if [ "$property_type" == "object" ]; then
+          echo "  $property: $nested_interface_name;" >> "$INTERFACE_FILE"
+          generate_interface "$prefix$property." "$json_file" "$nested_interface_name"
+        else
+          echo "  $property: $property_type;" >> "$INTERFACE_FILE"
+        fi
+      done <<< "$properties"
+      echo "}" >> "$INTERFACE_FILE"
+      ;;
+    *)
+      echo "export type $interface_name = $json_type;" >> "$INTERFACE_FILE"
+      ;;
+  esac
+}
+
+# Determine the final filename
+if [ -z "$FILENAME" ] && [ -z "$INTERFACE_NAME" ]; then
+  TIMESTAMP=$(date +%s)
+  FILENAME="/tmp/response_${TIMESTAMP}"
+elif [ -z "$FILENAME" ]; then
+  FILENAME="$INTERFACE_NAME"
+fi
+
+if [ -z "$INTERFACE_NAME" ]; then
+  INTERFACE_NAME="Response"
+fi
 
 # Create TypeScript interface file
-INTERFACE_FILE="/tmp/response_${TIMESTAMP}.ts"
+INTERFACE_FILE="/tmp/${FILENAME}.ts"
 
-echo "export interface Response {" > "$INTERFACE_FILE"
-echo -e "$INTERFACE" >> "$INTERFACE_FILE"
-echo "}" >> "$INTERFACE_FILE"
+# Generate TypeScript interface recursively
+generate_interface "" "$TMP_FILE" "$INTERFACE_NAME"
 
 echo "TypeScript interface generated: $INTERFACE_FILE"
