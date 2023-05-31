31280a1365153aff7a52d6e59cb8b175085640ac
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-05-31 11:49:59 -0400

    Add nested JSON support, including Arrays

--- ts_interface_from_curl.sh
+++ ts_interface_from_curl.sh
@@ -6,6 +6,7 @@ URL="https://example.com/api/endpoint"
 # Default values
 FILENAME=""
 INTERFACE_NAME=""
+INTERFACE_COUNT=1
 
 # Parse command-line arguments
 while getopts ":f:i:" opt; do
@@ -35,7 +36,7 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-# Generate TypeScript interface from nested JSON recursively
+# Generate TypeScript interface
 generate_interface() {
   local prefix=$1
   local json_file=$2
@@ -45,7 +46,7 @@ generate_interface() {
 
   case $json_type in
     "object")
-      local properties=$(jq -r 'keys | .[]' "$json_file")
+      local properties=$(jq -r 'keys_unsorted | .[]' "$json_file")
       echo "export interface $interface_name {" >> "$INTERFACE_FILE"
       while IFS= read -r property; do
         local property_type=$(jq -r ".$property | type" "$json_file")
@@ -60,6 +61,15 @@ generate_interface() {
       done <<< "$properties"
       echo "}" >> "$INTERFACE_FILE"
       ;;
+    "array")
+      local item_type=$(jq -r '.[0] | type' "$json_file")
+      local nested_interface_name="${interface_name}Item$INTERFACE_COUNT"
+
+      echo "export type $nested_interface_name = { $(jq -r '.[0] | to_entries | map("\(.key): \(.value | type)") | join(", ")' "$json_file") }" >> "$INTERFACE_FILE"
+      echo "export type $interface_name = $nested_interface_name[]" >> "$INTERFACE_FILE"
+
+      INTERFACE_COUNT=$((INTERFACE_COUNT + 1))
+      ;;
     *)
       echo "export type $interface_name = $json_type;" >> "$INTERFACE_FILE"
       ;;
@@ -69,7 +79,7 @@ generate_interface() {
 # Determine the final filename
 if [ -z "$FILENAME" ] && [ -z "$INTERFACE_NAME" ]; then
   TIMESTAMP=$(date +%s)
-  FILENAME="/tmp/response_${TIMESTAMP}"
+  FILENAME="response_${TIMESTAMP}"
 elif [ -z "$FILENAME" ]; then
   FILENAME="$INTERFACE_NAME"
 fi
@@ -85,3 +95,4 @@ INTERFACE_FILE="/tmp/${FILENAME}.ts"
 generate_interface "" "$TMP_FILE" "$INTERFACE_NAME"
 
 echo "TypeScript interface generated: $INTERFACE_FILE"
+cat $INTERFACE_FILE
